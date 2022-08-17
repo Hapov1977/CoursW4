6401e8d8faed66fedee58eabc7f24f938af91c5f
1 <avhapov@gmail.com>
2022-08-17 19:52:05 +0300

    ошибка исправлена

--- project/container.py
+++ project/container.py
@@ -11,6 +11,6 @@ user_dao = UsersDAO(db.session)
 
 # Services
 genre_service = GenresService(dao=genre_dao)
-director_service = DirectorsService(dao=genre_dao)
-movie_service = MoviesService(dao=genre_dao)
-user_service = UsersService(dao=genre_dao)
+director_service = DirectorsService(dao=director_dao)
+movie_service = MoviesService(dao=movie_dao)
+user_service = UsersService(dao=user_dao)
